729582c4fa5d2be0d55f9c3200c829b750108b90
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-27 21:18:15 +0300

    Update Solution - 03. Passed or Failed.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/03. Passed or Failed/Solution - 03. Passed or Failed.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/03. Passed or Failed/Solution - 03. Passed or Failed.java
@@ -6,7 +6,7 @@ public class Example {
 
         double grade = Double.parseDouble(scanner.nextLine());
 
-        if (grade >= 3) {
+        if (grade >= 3.00) {
             System.out.println("Passed!");
         } else {
             System.out.println("Failed!");
