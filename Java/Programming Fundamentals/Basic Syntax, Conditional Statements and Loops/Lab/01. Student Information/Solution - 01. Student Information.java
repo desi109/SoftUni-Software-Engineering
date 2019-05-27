11f636fbeea01878cd935d50edbb5d8f41041a5e
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-27 20:50:18 +0300

    Create Solution - 01. Student Information.java

---
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/01. Student Information/Solution - 01. Student Information.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+
+    }
+}
