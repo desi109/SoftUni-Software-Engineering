4da90b2416be707d7d4874faa51278590e2462fb
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-28 21:33:29 +0300

    Create Solution - 01. Ages.java

---
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/01. Ages/Solution - 01. Ages.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        if (age >= 0 && age <= 2)
+            System.out.print("baby");
+        else if (age >= 3 && age <= 13)
+            System.out.print("child");
+        else if (age >= 14 && age <= 19)
+            System.out.print("teenager");
+        else if (age >= 20 && age <= 65)
+            System.out.print("adult");
+        else if (age >= 66)
+            System.out.print("elder");
+
+
+    }
+}
