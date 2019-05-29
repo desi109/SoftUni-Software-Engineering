a1e8f310daffcdc38f26164a8a65cb08c63a7ea9
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-29 09:56:28 +0300

    Create Solution - 06. Strong Number.java

---
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/06. Strong Number/Solution - 06. Strong Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String numberInput = scanner.nextLine();
+        long sum = 0;
+
+        for (int i = 0; i < numberInput.length(); i++) {
+            int number = Integer.parseInt(
+                    String.valueOf(numberInput.charAt(i)));
+
+            int factorial = 1;
+            for (int j = 1; j <= number; j++) {
+                factorial *= j;
+            }
+            sum += factorial;
+        }
+
+        System.out.println(numberInput.equals(
+                String.valueOf(sum)) ? "yes" : "no");
+
+    }
+}
