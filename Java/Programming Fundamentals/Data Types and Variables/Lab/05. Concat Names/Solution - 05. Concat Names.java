4b1d82e1980551b35c971b7b64d2ad955a93493f
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-30 18:28:25 +0300

    Update Solution - 05. Concat Names.java

--- Java/Programming Fundamentals/Data Types and Variables/Lab/05. Concat Names/Solution - 05. Concat Names.java
+++ Java/Programming Fundamentals/Data Types and Variables/Lab/05. Concat Names/Solution - 05. Concat Names.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name1 = scanner.nextLine();
+        String name2 = scanner.nextLine();
+        String symbol = scanner.nextLine();
+
+        System.out.println(name1 + symbol + name2);
+
+    }
+}
