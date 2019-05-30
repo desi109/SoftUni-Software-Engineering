6e3ee1fd0be2809345caf31ce900c7d7dff3ca6f
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-30 18:22:29 +0300

    Update Solution - 07. Reversed Chars.java

--- Java/Programming Fundamentals/Data Types and Variables/Lab/07. Reversed Chars/Solution - 07. Reversed Chars.java
+++ Java/Programming Fundamentals/Data Types and Variables/Lab/07. Reversed Chars/Solution - 07. Reversed Chars.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char letter1 = scanner.next().charAt(0);
+        char letter2 = scanner.next().charAt(0);
+        char letter3 = scanner.next().charAt(0);
+
+        System.out.printf("%c %c %c", letter3, letter2,letter1);
+
+
+    }
+}
