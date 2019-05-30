6a76f89f8d2e3c1e744f61823b250f3a95ded29e
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-30 18:13:06 +0300

    Update Solution - 08. Lower or Upper.java

--- Java/Programming Fundamentals/Data Types and Variables/Lab/08. Lower or Upper/Solution - 08. Lower or Upper.java
+++ Java/Programming Fundamentals/Data Types and Variables/Lab/08. Lower or Upper/Solution - 08. Lower or Upper.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char letter = scanner.next().charAt(0);
+
+        if (Character.isLowerCase(letter)) {
+            System.out.println("lower-case");
+        } else {
+            System.out.println("upper-case");
+        }
+
+    }
+}
