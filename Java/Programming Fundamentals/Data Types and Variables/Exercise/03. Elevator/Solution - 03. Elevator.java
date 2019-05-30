e0c46da8b0ed47213a7c32953468d09fea6bacd0
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-30 21:59:29 +0300

    Update Solution - 03. Elevator.java

--- Java/Programming Fundamentals/Data Types and Variables/Exercise/03. Elevator/Solution - 03. Elevator.java
+++ Java/Programming Fundamentals/Data Types and Variables/Exercise/03. Elevator/Solution - 03. Elevator.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int p = Integer.parseInt(scanner.nextLine());
+
+        int courses = (int) Math.ceil((double) n / p);
+        System.out.println(courses);
+
+    }
+}
