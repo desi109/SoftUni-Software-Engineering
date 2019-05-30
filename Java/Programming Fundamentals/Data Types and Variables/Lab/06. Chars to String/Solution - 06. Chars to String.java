64f4abbd72f4f295722d237f65aaf211f1634a5a
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-30 21:07:21 +0300

    Create Solution - 06. Chars to String.java

---
+++ Java/Programming Fundamentals/Data Types and Variables/Lab/06. Chars to String/Solution - 06. Chars to String.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char letter1 = scanner.next().charAt(0);
+        char letter2 = scanner.next().charAt(0);
+        char letter3 = scanner.next().charAt(0);
+
+        String answer = "";
+
+
+        System.out.println(answer + letter1 + letter2 + letter3);
+
+
+    }
+}
