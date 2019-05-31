22c4478e91242b38d940b89948a8d5b457e8ed88
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-05-31 09:06:42 +0300

    Update Solution - 04. Sum of Chars.java

--- Java/Programming Fundamentals/Data Types and Variables/Exercise/04. Sum of Chars/Solution - 04. Sum of Chars.java
+++ Java/Programming Fundamentals/Data Types and Variables/Exercise/04. Sum of Chars/Solution - 04. Sum of Chars.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        while (n-- > 0){
+            String letter = scanner.nextLine();
+            sum += letter.charAt(0);
+        }
+        System.out.printf("The sum equals: %d", sum);
+
+    }
+}
