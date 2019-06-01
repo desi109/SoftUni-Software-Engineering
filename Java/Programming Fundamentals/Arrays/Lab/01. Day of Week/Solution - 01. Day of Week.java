98a3a7101e66bbbdad9dbe9b74c7fc3aa228ef89
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-01 22:43:36 +0300

    Create Solution - 01. Day of Week.java

---
+++ Java/Programming Fundamentals/Arrays/Lab/01. Day of Week/Solution - 01. Day of Week.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String weekDays[] = {"Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"};
+
+        int num = Integer.parseInt(scanner.nextLine());
+        if (num >= 1 && num <= 7){
+            System.out.println(weekDays[num -1]);
+        }
+        else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
+
