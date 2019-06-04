30ac218d101103011b29179b1b96a53855df0937
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-04 07:35:38 +0300

    Create Solution - 07. NxN Matrix.java

---
+++ Java/Programming Fundamentals/Methods/Exercise/07. NxN Matrix/Solution - 07. NxN Matrix.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        print(a);
+    }
+
+    static void print(int a) {
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                System.out.printf("%d ", a);
+            }
+            System.out.println();
+        }
+
+    }
+}
