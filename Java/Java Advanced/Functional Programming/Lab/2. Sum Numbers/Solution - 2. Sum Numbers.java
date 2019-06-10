417eb23d79bb245c82c5c29715c831d2d46d45b1
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-10 19:39:01 +0300

    Create Solution - 2. Sum Numbers.java

---
+++ Java/Java Advanced/Functional Programming/Lab/2. Sum Numbers/Solution - 2. Sum Numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.function.Function;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(", ");
+
+
+        if (input.length < 2) {
+            System.out.println("Count = " + input.length);
+            System.out.println("Sum = " + input[0]);
+        } else {
+            Function<String, Integer> parser = x -> Integer.parseInt(x);
+            int sum = 0;
+            for (String s : input) sum += parser.apply(s);
+            System.out.println("Count = " + input.length);
+            System.out.println("Sum = " + sum);
+        }
+
+
+    }
+}
+
