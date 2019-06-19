eaaccc6ad0eaa08f9370effbc875ba4066b4a05b
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-19 10:00:13 +0300

    Create Articles.java

---
+++ Java/Programming Fundamentals/Objects and Classes/Exercise/02. Articles/Articles.java
@@ -0,0 +1,41 @@
+public class Articles {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Articles(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void edit(String content) {
+        this.content = content;
+    }
+
+    public void changeAuthor(String author) {
+        this.author = author;
+    }
+
+    public void rename(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s: %s", this.getTitle(),
+                this.getContent(), this.getAuthor());
+    }
