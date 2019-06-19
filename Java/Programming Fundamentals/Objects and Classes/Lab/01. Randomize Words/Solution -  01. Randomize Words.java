bb5ba04587fce7087460cfb30284ed2e1383448e
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-19 09:30:45 +0300

    Create Solution -  01. Randomize Words.java

---
+++ Java/Programming Fundamentals/Objects and Classes/Lab/01. Randomize Words/Solution -  01. Randomize Words.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> words = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .collect(Collectors.toList());
+
+        while (!words.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(words.size());
+
+            String word = words.get(randomIndex);
+            System.out.println(word);
+            words.remove(word);
+        }
+    }
+}
