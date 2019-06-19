190ae07d9aef1d117c4ce09d5bf8b2aefc1cda42
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-19 09:41:21 +0300

    Create Songs.java

---
+++ Java/Programming Fundamentals/Objects and Classes/Lab/04. Songs/Songs.java
@@ -0,0 +1,28 @@
+public class Song {
+    private String typeList;
+    private String name;
+    private String time;
+
+    public String getTypeList() {
+        return typeList;
+    }
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime(String time) {
+        return this.time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
