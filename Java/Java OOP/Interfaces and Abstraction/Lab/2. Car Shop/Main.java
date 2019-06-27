8b8befef31768686693b45b9699af8d6ba188a79
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-06-27 09:49:16 +0300

    Create Main.java

---
+++ Java/Java OOP/Interfaces and Abstraction/Lab/2. Car Shop/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Seat("Spain", "Leon", "grey", 110);
+
+        System.out.println(car.toString());
+
+    }
+}
