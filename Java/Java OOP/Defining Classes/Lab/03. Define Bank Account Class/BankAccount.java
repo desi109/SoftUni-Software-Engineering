fe993d6bd2aaa3e0a0014045f5bf631e1a859376
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 22:20:01 +0300

    Create BankAccount.java

---
+++ Java/Java OOP/Defining Classes/Lab/03. Define Bank Account Class/BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+
+    public int id;
+    public double balance;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
+
