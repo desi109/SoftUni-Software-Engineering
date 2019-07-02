426a01dd75d9c8bb349b59542429e37192c77f6d
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 22:42:06 +0300

    Create Main.java

---
+++ Java/Java OOP/Defining Classes/Lab/Main.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+
+        BankAccount ba = new BankAccount();
+        ba.id = 1;
+        ba.balance = 15;
+
+        System.out.printf("Account ID%d, balance %.2f",
+                ba.getId(),
+                ba.getBalance());
+
+    }
+}
