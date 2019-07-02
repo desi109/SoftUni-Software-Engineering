c46eb27020b6911e21effea9f13ee32809ca8fbe
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 12:32:09 +0300

    Create Main.java

---
+++ Java/Java OOP/Generics/Lab/04. List Utilities/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+
+        List<Integer> integers = new ArrayList<>();
+        Collections.addAll(integers,1, 2, 18, 2, -1);
+
+        Integer maxInteger = ListUtils.getMax(integers);
+        System.out.println(maxInteger);
+
+
+        List<String> strings = new ArrayList<>();
+        Collections.addAll(strings, "a", "b", "c");
+
+        String minString = ListUtils.getMin(strings);
+        System.out.println(minString);
+    }
+}
+
+
