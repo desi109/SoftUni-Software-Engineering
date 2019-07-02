e23bd32eb6618924616adb82ce1441c74c784847
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 23:07:48 +0300

    Create Person.java

---
+++ Java/Java OOP/Defining Classes/Exercises/1. Define Class Person/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private int age;
+    private String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
