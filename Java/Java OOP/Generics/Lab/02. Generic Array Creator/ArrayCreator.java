a2f77b37eb0e4132ffee702179a53757d104a626
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 11:39:19 +0300

    Create ArrayCreator.java

---
+++ Java/Java OOP/Generics/Lab/02. Generic Array Creator/ArrayCreator.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+public class ArrayCreator<T> {
+
+    public static <T> T[] create(int length, T item) {
+        T[] arr = (T[]) new Object();
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = item;
+        }
+        return arr;
+    }
+
+
+    public static <T> T[] create(Class<T> cl, int length, T item) {
+        T[] arr = (T[]) Array.newInstance(cl, length);
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = item;
+        }
+        return arr;
+    }
+}
