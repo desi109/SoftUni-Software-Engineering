c0902e4301da1e08c948280cd2d6f63bc049d4cc
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-02 20:50:58 +0300

    Create Main.java

---
+++ Java/Java OOP/Generics/Lab/06. Generic Flat Method/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        Collections.addAll(integers,1, 2, 3);
+
+        List<Double> doubles = new ArrayList<>();
+        Collections.addAll(doubles,1.2, 3.2);
+
+        List<List<? extends  Number>> jagged = new ArrayList<>();
+
+        List<Number> dest = new ArrayList<>();
+        ListUtils.flatten(dest, jagged);
+
+    }
+}
