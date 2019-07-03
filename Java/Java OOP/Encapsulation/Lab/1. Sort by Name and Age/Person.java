28226472a17a9e03fcc72c6128cea0888cf64fb6
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-07-03 22:18:07 +0300

    Create Person.java

---
+++ Java/Java OOP/Encapsulation/Lab/1. Sort by Name and Age/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb
+                .append(String.format("%s %s is %d years old.",
+                        this.getFirstName(),
+                        this.getLastName(),
+                        this.getAge()));
+
+        return sb.toString();
+    }
+
+}
