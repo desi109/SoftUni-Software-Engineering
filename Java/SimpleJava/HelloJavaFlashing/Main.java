60f26079e93922aead76011fe8ca8b2b0433c3e4
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-07 07:16:02 +0300

    Create Main.java

---
+++ Java/SimpleJava/HelloJavaFlashing/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloJava");
+        frame.add(new HelloComponent("Hello, Java!"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
+
