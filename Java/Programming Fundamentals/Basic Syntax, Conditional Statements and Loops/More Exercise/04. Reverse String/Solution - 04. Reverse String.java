f95b1c4d1b013bd4de3e6dd2b4f9a2879195d159
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-16 13:26:32 +0300

    Update Solution - 04. Reverse String.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String/Solution - 04. Reverse String.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String/Solution - 04. Reverse String.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        for (int i = line.length() - 1; i >= 0; i--) {
+            System.out.print(line.charAt(i));
+        }
+    }
+}
