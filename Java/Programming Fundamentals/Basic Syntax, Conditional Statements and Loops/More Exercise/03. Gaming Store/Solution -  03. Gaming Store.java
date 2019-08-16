e3c7764f6c4fe3e664b6bc56853e170d475bb1f2
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-16 12:53:10 +0300

    Update Solution -  03. Gaming Store.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/03. Gaming Store/Solution -  03. Gaming Store.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/03. Gaming Store/Solution -  03. Gaming Store.java
@@ -1,1 +1,50 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        HashMap<String, Double> games = new HashMap<String, Double>();
+
+        // Add keys and values (Game, Price)
+        games.put("OutFall 4", 39.99);
+        games.put("CS: OG", 15.99);
+        games.put("Zplinter Zell", 19.99);
+        games.put("Honored 2", 59.99);
+        games.put("RoverWatch", 29.99);
+        games.put("RoverWatch Origins Edition", 39.99);
+        
+        double balance = Double.parseDouble(scanner.nextLine());
+        double startBalance = balance;
+
+        String line;
+        String print;
+
+        while (!(line = scanner.nextLine()).equals("Game Time")) {
+
+            if (games.containsKey(line)) {
+                if (balance < games.get(line)) {
+                    print = "Too Expensive";
+                } else {
+                    balance -= games.get(line);
+                    print = "Bought " + line;
+                }
+            } else {
+                print = "Not Found";
+            }
+
+            System.out.println(print);
+        }
+
+
+        if (balance == 0) {
+            System.out.println("Out of mo-ney!");
+
+        } else {
+            System.out.println(String.format
+                    ("Total spent: $%.2f. Remaining: $%.2f",
+                            startBalance - balance, balance));
+        }
+    }
+}
