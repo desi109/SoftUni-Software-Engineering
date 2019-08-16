a28ef07338ce4dedc30360f6694c838d3a01727a
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-16 11:04:26 +0300

    Update Solution - 01. Sort Numbers.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/01. Sort Numbers/Solution - 01. Sort Numbers.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/01. Sort Numbers/Solution - 01. Sort Numbers.java
@@ -1,1 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = new ArrayList<Integer>();
+        for(int i = 0; i<= 2; i++){
+            numbers.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        Collections.sort(numbers);
+        for (int i = 2; i >= 0 ; i--) {
+            System.out.println(numbers.get(i));
+        }
+    }
+}
