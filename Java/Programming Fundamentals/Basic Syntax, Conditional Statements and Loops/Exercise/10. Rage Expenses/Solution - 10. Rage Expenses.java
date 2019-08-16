a395e8c94482b3472845015ee88880dfd21cb01f
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-16 15:58:03 +0300

    Update Solution - 10. Rage Expenses.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/10. Rage Expenses/Solution - 10. Rage Expenses.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/10. Rage Expenses/Solution - 10. Rage Expenses.java
@@ -1,1 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int lostCount = Integer.parseInt(scanner.nextLine());
+
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+
+        int trashedHeadset = 0;
+        int trashedMouse = 0;
+        int trashedKeyboards = 0;
+        int trashedDisplays = 0;
+
+        for (int a = 1; a <= lostCount; a++) {
+            if (a % 2 == 0) {
+                trashedHeadset++;
+            }
+            if (a % 3 == 0) {
+                trashedMouse++;
+            }
+            if (a % 2 == 0 && a % 3 == 0) {
+                trashedKeyboards++;
+                if (trashedKeyboards % 2 == 0) {
+                    trashedDisplays++;
+                }
+            }
+        }
+
+        double result = (trashedHeadset*headsetPrice) +
+                (trashedMouse*mousePrice) +
+                (trashedKeyboards*keyboardPrice) +
+                (trashedDisplays*displayPrice);
+
+        System.out.printf("Rage expenses: %.2f lv.",result);
+    }
+}
