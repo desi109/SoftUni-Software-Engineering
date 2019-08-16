ebc53ace0830a80cc3a822fcb518d50482dadc48
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-16 16:09:27 +0300

    Update Solution - 09. Padawan Equipment.java

--- Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/09. Padawan Equipment/Solution - 09. Padawan Equipment.java
+++ Java/Programming Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/09. Padawan Equipment/Solution - 09. Padawan Equipment.java
@@ -1,1 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        double moneyAmount = Double.parseDouble(scanner.nextLine());
+        int countOfStudents = Integer.parseInt(scanner.nextLine());
+        double priceOfLightsaber = Double.parseDouble(scanner.nextLine());
+        double robePrice = Double.parseDouble(scanner.nextLine());
+        double beltPrice = Double.parseDouble(scanner.nextLine());
+
+        int neededSabers = countOfStudents + (int) Math.ceil((double) countOfStudents * 0.1);
+        int freeBelts = countOfStudents / 6;
+        double totalSum = priceOfLightsaber * neededSabers +
+                robePrice * countOfStudents +
+                beltPrice * (countOfStudents - freeBelts);
+
+        if (totalSum <= moneyAmount) {
+            System.out.println(
+                    String.format("The moneyAmount is enough - it would cost %.2flv.%n",
+                            totalSum));
+        } else {
+            System.out.println(
+                    String.format("Ivan Cho will need %.2flv more.%n",
+                            totalSum - moneyAmount));
+        }
+
+    }
+}
