44464bcc852ec9a0a3f9d733cd328ca4ea2349b9
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-18 07:19:08 +0300

    Create Solution - 11. Snowballs.java

---
+++ Java/Programming Fundamentals/Data Types and Variables/Exercise/11. Snowballs/Solution - 11. Snowballs.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keys = new Scanner(System.in);
+
+        int n = Integer.parseInt(keys.nextLine());
+        double snowballValue = 0;
+        double maxSnowballValue = 0;
+        int snowballSnowOut = 0;
+        int snowballTimeOut = 0;
+        int snowballQualityOut = 0;
+
+        for (int i = 0; i < n; i++) {
+            int snowballSnow = Integer.parseInt(keys.nextLine());
+            int snowballTime = Integer.parseInt(keys.nextLine());
+            int snowballQuality = Integer.parseInt(keys.nextLine());
+
+            snowballValue = Math.pow((snowballSnow / snowballTime), snowballQuality);
+            
+            if (snowballValue > maxSnowballValue) {
+                maxSnowballValue = snowballValue;
+                snowballSnowOut = snowballSnow;
+                snowballTimeOut = snowballTime;
+                snowballQualityOut = snowballQuality;
+
+            }
+
+        }
+        System.out.printf("%d : %d = %.0f (%d)", 
+                snowballSnowOut, snowballTimeOut, maxSnowballValue, snowballQualityOut);
+
+    }
+}
