220f2b601e1d4174870d1d326894292ecb50698c
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-08-22 14:48:48 +0300

    Create Main.java

---
+++ Java/HackerRank/PracticeInterview/Preparation Kit/Warm-up Challenges/RepeatedString/Main.java
@@ -0,0 +1,43 @@
+//Problem: https://www.hackerrank.com/challenges/repeated-string/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long quotient = n / (s.length());
+        long remainder = n % (s.length());
+        long count = 0;
+
+        if ((s.length() == 1) && (s.charAt(0) == 'a')) {
+            return n;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                count++;
+            }
+        }
+        count = count * quotient;
+        for (int i = 0; i < remainder; i++) {
+            if (s.charAt(i) == 'a') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        long n = scanner.nextLong();
+
+        long result = repeatedString(s, n);
+
+        System.out.println(result);
+    }
+}
